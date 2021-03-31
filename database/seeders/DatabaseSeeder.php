8c01fc93ba0e69c58b2543d632ee75fca28871e0
Vladimir <verdant24@yandex.ru>
2021-03-31 09:39:53 +0300

    создали модель и миграцию ролей пользователей, добавили роли в базу данных

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Database\Seeders\UsersTableSeeder;
+use Database\Seeders\RoleSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,7 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            UsersTableSeeder::class
+            UsersTableSeeder::class,
+            RoleSeeder::class
         ]);
     }
 }
